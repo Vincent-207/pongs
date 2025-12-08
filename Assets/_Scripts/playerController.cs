9b82a505a75b7aa91c22c7c34ae5f83c81059345
Vincent-207 <vhannah2007@outlook.com>
2025-12-07 20:58:31 -0600

    Player bounds

--- Assets/_Scripts/playerController.cs
+++ Assets/_Scripts/playerController.cs
@@ -6,11 +6,22 @@ public class playerController : MonoBehaviour
     [SerializeField]
     InputActionReference move;
     [SerializeField]
-    float moveSpeed, bottomBound, TopBound;
+    float moveSpeed, bottomBound, topBound;
     void Update()
     {
         float moveInput = move.action.ReadValue<Vector2>().y;
         transform.position = transform.position + (Vector3.up * moveInput * moveSpeed * Time.deltaTime);
-        
+        applyBounds();
+    }
+    void applyBounds()
+    {
+        if(transform.position.y > topBound)
+        {
+            transform.position = new Vector2(transform.position.x, topBound);
+        }
+        else if(transform.position.y < bottomBound)
+        {
+            transform.position = new Vector2(transform.position.x, bottomBound);
+        }
     }
 }
